c14ab8c77bc85fa4b5edbc42db03a254686f73ce
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-06-25 12:38:04 -0400

    Included pirmary colour from main websites

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        day: "#f4f9f9",
+        night: "#141819",
+        easternblue: {
+          50: "#f1fafa",
+          100: "#dbf1f2",
+          200: "#aedfe2",
+          300: "#8bcfd5",
+          400: "#54b3bc",
+          500: "#3997a1",
+          600: "#327b88",
+          700: "#2e6570",
+          800: "#2d545d",
+          900: "#294750",
+          950: "#172e35",
+        },
+      },
+    },
   },
   plugins: [],
 }
