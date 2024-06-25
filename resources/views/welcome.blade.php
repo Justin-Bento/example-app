62f0c892d96ad3ea454e148163ccb3bb29216c70
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-06-25 12:33:10 -0400

    Copied the main layout of my website from my portfolio.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,21 @@
     @vite('resources/css/app.css')
 </head>
 
-<body class="p-16">
+<body class="bg-green-50/50">
     <header class="ring h-20">
     </header>
-    <main class="my-12 ring min-h-dvh">
+    <main class="grid place-content-center ring min-h-dvh">
+        <section class="max-w-7xl mx-auto text-center">
+            <div class="lg:max-w-[80ch] space-y-4">
+                <p>Hi, My Name is Justin Bento</p>
+                <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-balance">From Designs to Dynamic Websites: Join Me On My Coding Adventure!</h1>
+                <p class="leading-7 lg:text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptas amet corrupti quidem obcaecati possimus sed dolorum mollitia delectus ipsum, facilis dolores quod? Mollitia, maxime repudiandae? Voluptatum, quia excepturi. Dolores.</p>
+            </div>
+            <span class="flex flex-col lg:flex-row gap-4 lg:justify-center mt-4">
+                <button type="button" class="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Explore My Portfolio</button>
+                <button type="button" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Let's Connect</button>
+            </span>
+        </section>
     </main>
     <footer class="ring h-20">
     </footer>
